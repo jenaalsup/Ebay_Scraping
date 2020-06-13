b106e29451de182942a082505d6b42b9ba2d4a10
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-12 22:31:08 -0700

    Scrape ebay sold items

--- ebay_scraper.py
+++ ebay_scraper.py
@@ -1,21 +1,23 @@
 import argparse
+import requests
+from lxml import html
+from price_parser import Price
 #from pprint import pprint
 #from traceback import format_exc
-
-import requests
-# import unicodecsv as csv
-from lxml import html
-
-from price_parser import Price
+#import unicodecsv as csv
 
 # keeps track of global data stats
+
 stats = ""
+stats_sold = ""
 scraped_products = []
+available_value = 0
+sold_value = 0
 
-
-def parse(brand):
+def parse_available(brand):
     global stats
- 
+    global available_value
+
     page_num = 1
     scraped_products = []
     total_value = 0
@@ -76,44 +78,144 @@ def parse(brand):
             title = ' '.join(' '.join(raw_title).split())
             product_type = ''.join(raw_product_type)
             title = title.replace(product_type, '').strip()
+
             data = {
                         'url':raw_url[0],
                         'title':title,
-                        'price':price
+                        'price':price, 
+                        'sold':"Available"
             }
             scraped_products.append(data)
-
+        print("data: ", data)
         if scraped_products:
             total_count = total_count + count
             if count < 200:
                 value = (total_value / total_count) * int(result_count)
-                stats = "  STATS for Brand: %s, Items Scanned: %d, Total Items: %d Value: $%0.2f "%( brand, 
-                total_count, int(result_count), value)   
+                stats = "  AVAILABLE STATS for Brand: %s, Items Scanned: %d, Total Items (Including sponsored): %d Value (Without sponsored): $%0.2f "%( brand, 
+                total_count, int(result_count), value) 
+                available_value = value  
                 print(stats)
                 print("-------> DONE!")
                 break
             page_num = page_num + 1
     return scraped_products
 
+def parse_sold(brand):
+    global sold_stats
+    global sold_value
+ 
+    page_num = 1
+    scraped_products = []
+    total_value = 0
+    total_count = 0
+
+    while True:
+        #url = 'https://www.ebay.com/sch/i.html?_nkw="{0}"&_sacat=0&_dmd=1&_sop=1'.format(brand) - sorts by ending soonest
+        url = 'https://www.ebay.com/sch/i.html?_nkw="{0}"&_sacat=0&_dmd=1&_sop=10&LH_Complete=1&_ipg=200&_pgn={1}'.format(brand, page_num) # sorts by newly listed
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+        failed = False
+
+        # Retries for handling network errors
+        for _ in range(5):
+            print ("Retrieving %s\n\n"%(url)) 
+            response = requests.get(url, headers=headers, verify=True)
+            parser = html.fromstring(response.text)
+            print ("\n\nParsing page")
+
+            if response.status_code!=200:
+                failed = True
+                continue
+            else:
+                failed = False
+                break
+
+        if failed:
+            return []
+
+        #product_listings = parser.xpath('//li[contains(@id,"results-listing")]')
+        product_listings = parser.xpath('//li[contains(@class, "s-item    ")]')
+        raw_result_count = parser.xpath("//h1[contains(@class,'count-heading')]//text()")
+
+        if raw_result_count == None: 
+            print("NILNILNIL")
+        if len(raw_result_count) < 1:
+            print("0000000000")
+            continue
+        #result_count = ''.join(raw_result_count).strip()
+        result_count = raw_result_count[0]
+        result_count = result_count.replace(',', "")
+        #print ("Found {0} for {1}".format(result_count,brand))
+ 
+        count = 0
+        for product in product_listings:
+            raw_url = product.xpath('.//a[contains(@class,"item__link")]/@href')
+            raw_title = product.xpath('.//h3[contains(@class,"item__title")]//text()')
+            raw_product_type = product.xpath('.//h3[contains(@class,"item__title")]/span[@class="LIGHT_HIGHLIGHT"]/text()')
+            raw_price = product.xpath('.//span[contains(@class,"s-item__price")]//text()')
+
+            count = count + 1
+            price  = ' '.join(' '.join(raw_price).split())
+            parsed_price = Price.fromstring(price)
+            total_value = total_value + parsed_price.amount_float
+            title = ' '.join(' '.join(raw_title).split())
+            product_type = ''.join(raw_product_type)
+            title = title.replace(product_type, '').strip()
+
+            data = {
+                        'url':raw_url[0],
+                        'title':title,
+                        'price':price, 
+                        'sold':"Sold"
+            }
+            scraped_products.append(data)
+        print("data: ", data)
+        if scraped_products:
+            total_count = total_count + count
+            if count < 200:
+                value = (total_value / total_count) * int(result_count)
+                sold_stats = "  SOLD STATS for Brand: %s, Total Sold Items: %d, Value: $%0.2f "%( brand, 
+                total_count, value)   
+                print(sold_stats)
+                sold_value = value
+                print("-------> DONE!")
+                break
+            page_num = page_num + 1
+    return scraped_products
 
 def save_scraped_data(sdata, brand):
     file_name = str(brand) + "_result.csv"
     f = open(file_name,"w+")
-    f.write("\"title\", price, url\r\n")
-    test = {
+    f.write("\"title\", price, sold, url\r\n")
+
+    show_available_stats = {
                 'url':'',
                 'title':stats,
-                'price':''
+                'price':'',
+                'sold':''
             }
-    sdata.insert(0, test)
+    show_sold_stats = {
+                'url':'',
+                'title':sold_stats,
+                'price':'',
+                'sold':''
+            }
+    var = "  TOTAL VALUE OF AVAILABLE AND SOLD ITEMS: $" + str(available_value + sold_value)
+    show_total_value = {
+                'url':'',
+                'title':var,
+                'price':'',
+                'sold':''
+            }
+    sdata.insert(0, show_available_stats)
+    sdata.insert(1, show_sold_stats)
+    sdata.insert(2, show_total_value)
+
     for data in sdata:
-        #the_title = "\"", data['title'], "\""
         f.write("\"" + data['title'] + "\", ")
         new_price = data['price'].replace(',', "")
         f.write( new_price + ", ")
+        f.write( data['sold'] + ", ")
         f.write( data['url'] + "\r\n")
-    #for i in range(10):
-        #f.write("This is line %d\r\n" % (i+1))
     f.close() 
     return
 
@@ -150,7 +252,8 @@ if __name__=="__main__":
         process_file(file)
     else:
         brand = args.brand
-        scraped_data = parse(brand)
+        scraped_data = parse_available(brand)
+        scraped_data = scraped_data + parse_sold(brand)
         save_scraped_data(scraped_data, brand)
        
     # done
